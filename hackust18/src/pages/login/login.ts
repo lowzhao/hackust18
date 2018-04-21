05648c9de88c751ac185a576b2455c8adfafafb3
lowzhao <lowzhao@gmail.com>
2018-04-21 21:55:49 +0800

    adsad

--- hackust18/src/pages/login/login.ts
+++ hackust18/src/pages/login/login.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AngularFireAuth } from 'angularfire2/auth';
+import firebase from 'firebase';
 
 /**
  * Generated class for the LoginPage page.
@@ -10,16 +12,34 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html',
+	selector: 'page-login',
+	templateUrl: 'login.html',
 })
-export class LoginPage {
+export class LoginPage
+{
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+	constructor(public navCtrl: NavController, public navParams: NavParams,
+		private fire: AngularFireAuth)
+	{
+	}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
-  }
+	ionViewDidLoad()
+	{
+		console.log('ionViewDidLoad LoginPage');
+	}
+
+	loginWithFacebook()
+	{
+		this.fire.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider()).then(res =>
+		{
+			// console.log(res);
+		}
+		)
+	}
+
+	logoutOfFacebook()
+	{
+
+	}
 
 }
